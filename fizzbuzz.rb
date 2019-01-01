37213f2d1353b12c2469812ba03b9ae084adc214
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-01 14:25:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-    if x % 3 == 0 # if even divisible by 3
+    if x % 3 == 0 # if evenly divisible by 3
         "Fizz"
+    elsif x % 5 == 0 # else if evenly disible by 5
+        "Buzz"
     end
 end
